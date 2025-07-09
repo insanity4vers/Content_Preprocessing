d56f1355292cdce52aa6125455ab138ce9c72f5e
Manh <manhp0108@gmail.com>
2025-07-09 22:00:48 +0700

    Change function and variable name

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ def extract_content_and_url(series):
     contents = series.str.replace(r'(https?://\S+)', '', regex=True).str.strip()
     return contents, urls
 
-def add_or_replace_sale_soc(series):
-    mask_xemngay = series.str.startswith('Xem ngay')
-    series = series.mask(mask_xemngay, 'Sale SỐC' + series.str[len('Xem ngay'):])
-    mask_salesoc = series.str.startswith('Sale SỐC')
-    series = series.mask(~mask_salesoc, 'Sale SỐC' + series)
+def add_or_replace(series):
+    mask_org = series.str.startswith('Xem ngay')
+    series = series.mask(mask_org, 'SALE CỰC SHOCK MÚC NGAY THÔI!!!' + series.str[len('Xem ngay'):])
+    mask_new = series.str.startswith('SALE CỰC SHOCK MÚC NGAY THÔI!!!')
+    series = series.mask(~mask_new, 'SALE CỰC SHOCK MÚC NGAY THÔI!!!' + series)
     return series
 
 # Press the green button in the gutter to run the script.
@@ -25,7 +25,7 @@ if __name__ == '__main__':
         df = pd.read_excel(args.input)
         contents, urls = extract_content_and_url(df['content'].astype(str))
         content_full = contents.where(urls == '', contents + '\n' + urls)
-        content_full = add_or_replace_sale_soc(content_full)
+        content_full = add_or_replace(content_full)
         content_full = content_full.drop_duplicates()
         content_full.to_frame('content').to_excel(args.output, index=False)
         print(f"Đã xử lý xong và lưu kết quả {args.output}")
